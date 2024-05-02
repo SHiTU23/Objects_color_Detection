1c4592f9d23905c30e0bdfbe96ba37d2b3888cb5
SHiTU23 <shi.tu2323@gmail.com>
2024-05-02 16:28:05 +0330

    Fix: update the condition for detecting black objects

--- color_detection.py
+++ color_detection.py
@@ -5,7 +5,8 @@ def color(R, G, B):
         return 'Green'
     elif B> G and B > R:
         return 'Blue'
-    elif (R == 0 and B == 0 and G == 0) or (R==G and G== B and R == B):
+    elif ((R==G and G== B and R == B) or
+          (R == G or R == B or G == B)):
         return 'Black'
     else:
         return 'White'
